4cc8712e5239deda4828b3281186cb239a82a5c9
Mile1301 <119892913+Mile1301@users.noreply.github.com>
2023-10-27 12:31:59 +0200

    Update jwt.const.js

--- const/jwt.const.js
+++ const/jwt.const.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const createAccessToken = (userId) => {
-  return jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1s" });
+  return jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1m" });
 };
 export const verifyAccessToken = (token) => {
   return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
